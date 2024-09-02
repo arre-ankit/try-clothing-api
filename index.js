a5297cefa3e3e774ab54f9e162fc0288dbae64df
arre_ankit <arre.ankit@gmail.com>
2024-09-02 18:52:52 +0530

    fix:long running process

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fetch from 'node-fetch';
 import { Client } from '@gradio/client';
 
 const app = express();
@@ -8,22 +9,35 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// Helper function to add timeout to fetch requests
+async function fetchWithTimeout(url, options, timeout = 5000) {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(url, { ...options, signal: controller.signal });
+    clearTimeout(id);
+    return response;
+}
+
 app.post('/process-images', async (req, res) => {
   try {
-    // Fetch images from URLs in the request body
     const { amazon_img_url, model_img_url } = req.body;
-    
-    const response0 = await fetch(amazon_img_url);
-    const exampleImage = await response0.blob();
 
-    const response1 = await fetch(model_img_url);
-    const exampleImage_a = await response1.blob();
+    // Fetch images with timeout
+    const response0 = await fetchWithTimeout(amazon_img_url, {}, 5000); // 5 seconds timeout
+    const exampleImage = await response0.arrayBuffer();
+
+    const response1 = await fetchWithTimeout(model_img_url, {}, 5000); // 5 seconds timeout
+    const exampleImage_a = await response1.arrayBuffer();
+
+    // Convert ArrayBuffer to Buffer
+    const bufferImage = Buffer.from(exampleImage);
+    const bufferImage_a = Buffer.from(exampleImage_a);
 
     // Connect to Gradio client
     const client = await Client.connect("Kwai-Kolors/Kolors-Virtual-Try-On");
     const result = await client.predict("/tryon", { 
-      person_img: exampleImage,
-      garment_img: exampleImage_a,
+      person_img: bufferImage,
+      garment_img: bufferImage_a,
       seed: 0,
       randomize_seed: true,
     });
