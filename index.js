25eb3bb22b18c34d2a3dd9e67c221e2fc78748d4
arre_ankit <arre.ankit@gmail.com>
2024-09-06 15:00:10 +0530

    changed amazon link logic

--- index.js
+++ index.js
@@ -26,53 +26,14 @@ const fetchImageAsBlob = async (url) => {
   }
 };
 
-const extractUrl = (text) => {
-  // Regular expression to match URLs
-  const amazonUrlPattern = /(https:\/\/(amzn\.in|www\.amazon\.in)\/[^\s]+)/;
-  const match = text.match(amazonUrlPattern);
-  return match ? match[0] : null;
-};
-
-const getFullUrl = async (shortenedUrl) => {
-  try {
-    const response = await axios.get(shortenedUrl, {
-      maxRedirects: 0,
-      validateStatus: status => status >= 200 && status < 400
-    });
-    return response.headers.location;
-  } catch (error) {
-    if (error.response && error.response.headers.location) {
-      return error.response.headers.location;
-    } else {
-      console.error('Error fetching the URL:', error.message);
-      return null;
-    }
-  }
-};
-
 
 app.post('/process-images', async (req, res) => {
   try {
     let { amazon_img_url, model_img_url } = req.body;
 
-    if (amazon_img_url) {
-      const url = extractUrl(amazon_img_url);
-      //console.log('Extracted Amazon URL:', url);
-      if (url) {
-        amazon_img_url = await getFullUrl(url);
-        //console.log('Amazon URL:', amazon_img_url);
-      }
-    }
-
     // Fetch the Amazon page and extract the image URL with a timeout
     const amazonResponse = await axios.get(amazon_img_url, {
       timeout: 5000, // 5 seconds timeout
-      headers: {
-        // Include necessary headers
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Pixel 3 XL Build/QQ1D.200205.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Referer': 'https://www.amazon.in',
-      }
     });
 
     const htmlContent = amazonResponse.data;
